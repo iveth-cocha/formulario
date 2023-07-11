786cd5b855e54e0e686bd6d13be294b2de398046
iveth-cocha <maria.iveth16@outlook.es>
2023-07-10 20:03:48 -0500

    opcion de guardar

--- src/formulario.java
+++ src/formulario.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.io.*;
 
 public class formulario {
     private JPanel rootPanel;
@@ -9,10 +11,47 @@ public class formulario {
     private JTextField age;
     private JButton cargarButton;
     private JButton guardarButton;
+
 public formulario() {
     guardarButton.addActionListener(new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
+            String filePath = "datos.dat";
+
+            name.setText(name.getText());
+            ci.setText(ci.getText());
+            age.setText(age.getText());
+
+            String [] info={name.getText(),ci.getText(),age.getText()};
+
+            try (FileOutputStream fileOut=new FileOutputStream(filePath);
+                 ObjectOutputStream obOut=new ObjectOutputStream(fileOut)){
+
+                obOut.writeObject(info);
+                System.out.println("archivo escrito correctamente");
+
+            }catch (IOException m){
+                throw new RuntimeException(m);
+            }
+            /*---------------------------------------------*/
+            /*
+            try(
+                    FileInputStream fileIn=new FileInputStream(filePath);
+                    ObjectInputStream obIn= new ObjectInputStream(fileIn)
+            ){
+                info readObject=(info)obIn.readObject();
+                System.out.println("El objeto en disco es: "+readObject);
+                System.out.println(readObject.name.);
+                System.out.println(readObject.getNombre());
+            }catch (IOException a){
+                throw new RuntimeException(a);
+            }catch (ClassNotFoundException b){
+                throw new RuntimeException(b);
+            }
+            */
+
+
+
 
         }
     });
@@ -21,6 +60,8 @@ public formulario() {
         public void actionPerformed(ActionEvent e) {
 
         }
+    });
+    rootPanel.addComponentListener(new ComponentAdapter() {
     });
 }
     public static void main(String[] args) {
